943c93907068d30e8f5b9ded5ac3bae5abd04aa1
= <faainamacaldera16@gmail.com>
2023-04-26 19:40:02 +0700

    feat(Login) Adding Login page and little bit fixing in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
 import Analytic from "./pages/dashboard/Analytic";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Analytic />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
