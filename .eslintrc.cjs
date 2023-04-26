03858297c66ac3933124f88e981227cc99a5d143
= <faainamacaldera16@gmail.com>
2023-04-26 16:18:51 +0700

    feat(Init Project): Make Frontend Scratch Project

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: [
+    "react-refresh",
+    "react",
+    "simple-import-sort",
+    "prettier",
+    "react-hooks",
+  ],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/accessible-emoji": "off",
+    "react/prop-types": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+  },
+};
